ccbe5b6372cfdc71df05532f18d3beee9246125f
erkinkirdan <erkinkirdan@users.noreply.github.com>
2025-05-30 23:06:56 +0200

    Update pub.py

--- pub.py
+++ pub.py
@@ -1,24 +1,18 @@
 import paho.mqtt.client as mqtt
 import time
 
-BROKER = "test.mosquitto.org"
-TOPIC = "test/topic"
+TOPIC = "kgortlswe"
 
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.connect(BROKER)
-
-
 def sleep(timing_bitstring, sleep_index):
     if timing_bitstring[sleep_index] == "1":
         time.sleep(1)
     else:
-        time.sleep(0.5)
+        time.sleep(0.1)
     return (sleep_index + 1) % len(timing_bitstring)
 
 
@@ -38,10 +32,14 @@ def string_to_bitstring(s):
 
 
 if __name__ == "__main__":
+    broker = input("Enter broker: ")
     storage_secret = input("Enter storage secret: ")
     timing_secret = input("Enter timing secret: ")
     storage_bitstring = string_to_bitstring(storage_secret)
     timing_bitstring = string_to_bitstring(timing_secret)
     timing_bitstring = timing_bitstring + "11111111"
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.connect(broker)
     client.loop_start()
     publish(storage_bitstring, timing_bitstring)
